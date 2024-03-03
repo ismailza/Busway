89f3bf3d1f768797d5d96656c8fe799f432bb5d6
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-03 15:19:38 +0100

    first commit

--- src/main/java/ma/fstm/ilisi/busway/controller/BuswayController.java
+++ src/main/java/ma/fstm/ilisi/busway/controller/BuswayController.java
@@ -4,11 +4,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ma.fstm.ilisi.busway.bo.*;
 import ma.fstm.ilisi.busway.service.BusService;
 import ma.fstm.ilisi.busway.service.StationService;
 import ma.fstm.ilisi.busway.service.VoyageService;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BuswayController extends HttpServlet {
     private BusService busService;
@@ -21,15 +25,63 @@ public class BuswayController extends HttpServlet {
         busService = new BusService();
         voyageService = new VoyageService();
         stationService = new StationService();
+        setupSomeTrips();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        switch (req.getServletPath()) {
+            case "/install" -> {
+                req.setAttribute("buses", this.busService.retreive());
+                req.getRequestDispatcher("installInfoBus.jsp").forward(req, resp);
+            }
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
+
+    /**
+     * Installer infos busway
+     * @param numBus Numéro du bus
+     * @param placesLimite Nombre de places limite du bus
+     * @param tarif Le tarif du voyage
+     * @param prenomConducteur Prénom du conducteur
+     * @param nomConducteur Nom du conducteur
+     * @param heureDepart L'heure de départ
+     * @param heureArrivee L'heure d'arrivée
+     * @param depart La station de départ
+     * @param arrivee La station d'arrivée
+     * @param arretes Les stations d'arrêtes
+     */
+    public void installerInfosBusway(int numBus, int placesLimite, float tarif,
+                                     String prenomConducteur, String nomConducteur,
+                                     LocalTime heureDepart, LocalTime heureArrivee,
+                                     String depart, String arrivee, Map<String, LocalTime> arretes) {
+        Station stationDepart = this.stationService.findByName(depart);
+        Station stationArrivee = this.stationService.findByName(arrivee);
+        if (stationDepart == null || stationArrivee == null)
+            return;
+        Bus bus = new Bus(numBus, placesLimite, new Conducteur(prenomConducteur, nomConducteur));
+        this.busService.create(bus);
+        Voyage voyage = new Voyage(tarif, bus, heureDepart, heureArrivee, stationDepart, stationArrivee);
+        for (Map.Entry<String, LocalTime> entry : arretes.entrySet()) {
+            Station stationArrete = this.stationService.findByName(entry.getKey());
+            if (stationArrete != null) {
+                voyage.addArrete(new Arrete(stationArrete, entry.getValue()));
+            }
+        }
+        this.voyageService.create(voyage);
+    }
+
+    private void setupSomeTrips() {
+        Map<String, LocalTime> arretes = new HashMap<>();
+        arretes.put("Station 2", LocalTime.of(7,15));
+        arretes.put("Station 3", LocalTime.of(7, 30));
+        arretes.put("Station 4", LocalTime.of(7, 45));
+        this.installerInfosBusway(1, 80, 6, "Ismail", "ZAHIR", LocalTime.of(7,0), LocalTime.of(8,0), "Station 1", "Station 5", arretes);
+    }
+
 }
---
+++ src/main/webapp/installInfoBus.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Busway</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="container">
+        <h1>Installer informations du bus</h1>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
