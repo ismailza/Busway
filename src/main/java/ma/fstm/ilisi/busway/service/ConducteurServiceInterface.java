e69699bb2cdd1d64bfa9454d6ed717f1ab488591
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-09 20:13:11 +0100

    add ConducteurServiceInterface

--- src/main/java/ma/fstm/ilisi/busway/service/ConducteurServiceInterface.java
+++ src/main/java/ma/fstm/ilisi/busway/service/ConducteurServiceInterface.java
@@ -2,9 +2,10 @@ package ma.fstm.ilisi.busway.service;
 
 import ma.fstm.ilisi.busway.bo.Conducteur;
 import ma.fstm.ilisi.busway.dto.ConducteurDTO;
+import ma.fstm.ilisi.busway.exception.DriverNotFoundException;
 
 public interface ConducteurServiceInterface extends ServiceInterface<ConducteurDTO> {
-    ConducteurDTO findById(Long id);
+    ConducteurDTO findById(Long id) throws DriverNotFoundException;
     Conducteur mapToConducteur(ConducteurDTO conducteurDTO);
     ConducteurDTO mapToConducteurDTO(Conducteur conducteur);
 }
