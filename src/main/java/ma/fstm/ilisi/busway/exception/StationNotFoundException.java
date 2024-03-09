13ebb162bce869468e4e009cf93d2f4ba881ffb9
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-09 16:48:02 +0100

    Add StationNotFoundException

---
+++ src/main/java/ma/fstm/ilisi/busway/exception/StationNotFoundException.java
@@ -0,0 +1,9 @@
+package ma.fstm.ilisi.busway.exception;
+
+public class StationNotFoundException extends Exception {
+
+    public StationNotFoundException(String message) {
+        super(message);
+    }
+
+}
