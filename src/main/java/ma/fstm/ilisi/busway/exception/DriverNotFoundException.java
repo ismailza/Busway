42e7a29a65c9ca62b26ee3859f4e816e8a8ba16c
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-09 16:47:51 +0100

    Add DriverNotFoundException

---
+++ src/main/java/ma/fstm/ilisi/busway/exception/DriverNotFoundException.java
@@ -0,0 +1,9 @@
+package ma.fstm.ilisi.busway.exception;
+
+public class DriverNotFoundException extends Exception {
+
+    public DriverNotFoundException(String message) {
+        super(message);
+    }
+
+}
