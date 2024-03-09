6adf8200b211ffed1a997e0820280b8794821433
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-09 16:47:37 +0100

    Add BusNotFoundException

---
+++ src/main/java/ma/fstm/ilisi/busway/exception/BusNotFoundException.java
@@ -0,0 +1,9 @@
+package ma.fstm.ilisi.busway.exception;
+
+public class BusNotFoundException extends Exception {
+
+    public BusNotFoundException(String message) {
+        super(message);
+    }
+
+}
