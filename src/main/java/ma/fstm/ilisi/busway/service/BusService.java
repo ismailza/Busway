1d2d7442e0e7f6a7bfc069764045f66bf68ba7a1
Ismail ZAHIR <ismailza407@gmail.com>
2024-03-09 13:05:45 +0100

    Add BusService

--- src/main/java/ma/fstm/ilisi/busway/service/BusService.java
+++ src/main/java/ma/fstm/ilisi/busway/service/BusService.java
@@ -1,37 +1,66 @@
 package ma.fstm.ilisi.busway.service;
 
 import ma.fstm.ilisi.busway.bo.Bus;
+import ma.fstm.ilisi.busway.dao.BusDAO;
+import ma.fstm.ilisi.busway.dto.BusDTO;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class BusService {
-    private static final List<Bus> buses;
+public class BusService implements BusServiceInterface {
 
-    static {
-        buses = new ArrayList<>(List.of(
-                new Bus(1, 90),
-                new Bus(2, 120),
-                new Bus(3, 110),
-                new Bus(4, 60),
-                new Bus(5, 30)
-        ));
+    private BusDAO busDAO;
+
+    public BusService(BusDAO busDAO) {
+        this.busDAO = busDAO;
     }
 
-    public void create(Bus bus) {
-        buses.add(bus);
+    @Override
+    public List<BusDTO> retreive() {
+        return this.busDAO.retreive().stream().map(this::mapToBusDTO).collect(Collectors.toList());
     }
 
-    public Bus findByNum(int numBus) {
-        for (Bus bus : buses) {
-            if (bus.getNumBus() == numBus)
-                return bus;
-        }
-        return null;
+    @Override
+    public boolean create(BusDTO bus) {
+        return this.busDAO.create(this.mapToBus(bus));
     }
 
-    public List<Bus> retreive() {
-        return buses;
+    @Override
+    public boolean update(BusDTO bus) {
+        return this.busDAO.update(this.mapToBus(bus));
+    }
+
+    @Override
+    public boolean delete(BusDTO bus) {
+        return this.busDAO.delete(this.mapToBus(bus));
+    }
+
+    @Override
+    public BusDTO findById(Long id) {
+        return this.mapToBusDTO(this.busDAO.findById(id));
+    }
+
+    @Override
+    public BusDTO findByBusNum(int numBus) {
+        return this.mapToBusDTO(this.busDAO.findByBusNum(numBus));
+    }
+
+    @Override
+    public Bus mapToBus(BusDTO busDTO) {
+        return new Bus(
+                busDTO.getId(),
+                busDTO.getNumBus(),
+                busDTO.getPlacesLimite()
+        );
+    }
+
+    @Override
+    public BusDTO mapToBusDTO(Bus bus) {
+        return new BusDTO(
+                bus.getId(),
+                bus.getNumBus(),
+                bus.getPlacesLimite()
+        );
     }
 
 }
